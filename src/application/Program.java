256701541652a1a3369a8287cbcd316aef6e1580
Rafael <JesusDoTI@github.com>
2020-12-18 11:26:27 -0300

    Implemented Department Class Entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,6 @@ import model.entities.Department;
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department dp = new Department(1, "Books");
-		
-		System.out.println(dp);
 	}
 
 }
