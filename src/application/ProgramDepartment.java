15123148ef59d835851734af256d7d064e63e32e
RafaelC4rdoso <rafaelgremista2010@gmail.com>
2021-01-21 14:54:57 -0300

    Department Update Implementation

--- src/application/ProgramDepartment.java
+++ src/application/ProgramDepartment.java
@@ -13,5 +13,12 @@ public class ProgramDepartment {
 		Department dp =  new Department(null, "Toys");
 		departmentDao.insert(dp);
 		System.out.println("Operation successfully completed! new ID: " + dp.getId());
+		
+		System.out.println("\n===  TEST 2: Department update ===\n");
+		dp.setId(6);
+		dp.setName("Home");
+		departmentDao.update(dp);
+		System.out.println("Updated successfully!");
+		
 	}
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -46,7 +46,20 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void update(Department dp) {
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE department "
+					+ "SET Name = ? "
+					+ "WHERE Id = ? ");
+			st.setString(1, dp.getName());
+			st.setInt(2, dp.getId());
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
